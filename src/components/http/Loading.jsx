1f37140b0a0823d763692b152a11adafcaa97c9f
Lucas Fernando <lucas98fernando@gmail.com>
2022-02-09 21:46:28 -0300

    Loading component improvements

--- src/components/http/Loading.jsx
+++ src/components/http/Loading.jsx
@@ -1,5 +1,13 @@
 import React from "react";
+import "./Pokemon.css";
 
 export default function Loading() {
-  return <div className="loading">Carregando...</div>;
+  return (
+    <img
+      src="https://cdn-icons-png.flaticon.com/512/528/528101.png"
+      className="pokeball"
+      alt="Loading"
+      width="50"
+    />
+  );
 }
